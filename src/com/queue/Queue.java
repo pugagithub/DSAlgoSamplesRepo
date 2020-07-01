372ced1cf7bed1f3a4cfbc9b8487d88c647e6987
pugal_git <pugal.sym@gmail.com>
2020-07-01 17:13:44 +0530

    Hi

--- src/com/queue/Queue.java
+++ src/com/queue/Queue.java
@@ -4,9 +4,20 @@ public class Queue {
 	
 	int[] arr = new int[20];
 	
-	int front;   // Remove From Front
 	
-	int rear;  // Insert From Back
+	
+	int front = 0;   // Remove From Front , Front is always 0
+	
+	int rear =-1;  // Insert From Back . Insert From i=len-1
+	
+	void insert(int data) {  // INSERT AT BACK
+		
+		arr[++rear]=data;
+	}
+	
+	int remove(){   // REMOVE FROM FRONT
+		return arr[front++];
+	}
 	
 	
 	
