7fd64678f9943038724b7680091a3c3ac4202b5b
pugal_git <pugal.sym@gmail.com>
2020-07-01 14:54:19 +0530

    Hello

--- src/com/stack/Stack.java
+++ src/com/stack/Stack.java
@@ -1,5 +1,22 @@
 package com.stack;
 
 public class Stack {
+	
+	int[] arr =new int[10];
+	int top=-1;
+	
+	
+	void push(int data) {
+		arr[++top] = data;
+	}
+	
+	int pop() {
+		return arr[top--];
+	}
+	
+	int peek() {
+		return arr[top];
+	}
+	
 
 }
--- src/com/tree/BinarySearchTree.java
+++ src/com/tree/BinarySearchTree.java
@@ -1,5 +1,18 @@
 package com.tree;
 
 public class BinarySearchTree {
+	
+	Node root;
+	
 
+}
+
+class Node {
+	
+	int data;
+	Node leftChild;
+	Node rightChild;
+	
+	Node(int data){this.data = data;}
+	
 }
