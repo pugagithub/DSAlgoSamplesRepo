201b4de47db112372c67624acd2f84ea1c0ca976
pugal_git <pugal.sym@gmail.com>
2020-07-01 17:53:17 +0530

    c

--- src/com/queue/Queue.java
+++ src/com/queue/Queue.java
@@ -17,8 +17,8 @@ public class Queue {
 		elemCount++;
 	}
 	
-	int remove(){
-		elemCount--;// REMOVE FROM FRONT
+	int remove(){  // REMOVE FROM FRONT
+		elemCount--;
 		return arr[front++];
 		
 	}
