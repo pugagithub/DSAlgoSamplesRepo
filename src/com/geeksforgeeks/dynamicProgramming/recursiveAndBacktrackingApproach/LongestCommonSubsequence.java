3ae794517f9beb2664796ac7a47273db53d093cf
pugal_git <pugal.sym@gmail.com>
2020-08-01 18:18:18 +0530

    Hello

--- src/com/geeksforgeeks/dynamicProgramming/recursiveAndBacktrackingApproach/LongestCommonSubsequence.java
+++ src/com/geeksforgeeks/dynamicProgramming/recursiveAndBacktrackingApproach/LongestCommonSubsequence.java
@@ -1,5 +1,51 @@
 package com.geeksforgeeks.dynamicProgramming.recursiveAndBacktrackingApproach;
 
 public class LongestCommonSubsequence {
+	
+	static String str1 = "ABCDGH";
+	static String str2 = "AEDFHR";
+	static int maxSubSeq = 0;
+	
+	public static void main(String[] args) {
+		
+		longestCommonSubSeq();
+		System.out.println("Max Sub Seq : "+ maxSubSeq);
+		
+	}
+	
+	static void longestCommonSubSeq(){
+		
+		for (int m = 0; m < str1.length(); m++) {
 
+			int tempJ = 0;
+			int count = 0;
+			String tempSubSeq = "";
+			for (int i = m; i < str1.length(); i++) {
+
+				for (int j = tempJ; j < str2.length(); j++) {
+
+					if (str1.charAt(i) == str2.charAt(j)) {
+
+						tempJ = j + 1;
+						count++;
+						tempSubSeq = tempSubSeq + str1.charAt(i);
+					}
+				}
+			}
+			
+			if(count > maxSubSeq) {
+				
+				System.out.println(" Temp Sub Seq : " + tempSubSeq);
+				maxSubSeq = count ;
+				
+			}
+
+		}
+		
+		
+	}
+
+	
+	
 }
+
